faa78ddac0cc7eececc2e2c1adf8cd41da7615c5
HardCoreWare <bernardocodu@gmail.com>
2019-05-14 17:55:34 -0500

    listo

--- app/core/app.php
+++ app/core/app.php
@@ -1,13 +1,22 @@
 <?php
 
+//
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+//
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true,'responseChunkSize' => 8096]]);
 
+//
 require_once '../app/core/container.php';
-require_once '../app/core/routes.php';
 
+//
+$app->get('/', \App\Controllers\TestController::class.':wellcome');
+$app->get('/hello/{name}', \App\Controllers\TestController::class.':hello');
+$app->get('/config', \App\Controllers\TestController::class.':config');
+$app->get('/database', \App\Controllers\TestController::class.':database');
+
+//
 $app->run();
 
 ?>
